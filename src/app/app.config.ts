ee7a94f8fc79ff0dfe9fdc213808084b3ba19b11
truongne <truonggolang@gmail.com>
2025-11-13 22:35:48 +0700

    initial

---
+++ src/app/app.config.ts
@@ -0,0 +1,15 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideHotToastConfig } from '@ngxpert/hot-toast';
+import { provideStore } from '@ngrx/store';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes), provideHotToastConfig(),
+    provideStore()
+]
+};
